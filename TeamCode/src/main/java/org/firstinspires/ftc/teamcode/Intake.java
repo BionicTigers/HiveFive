01160baf9db0a65d84626e4460948f4a31f9d844
BenjaminBeseler <23besebe@students.lovelandschools.org>
2021-11-03 19:30:11 -0400

    Intake save for swapping branches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -10,28 +10,29 @@ public class Intake extends Mechanism {
     public boolean goingout;
 
     //Creates, declares, and assigns a motor to the motors array list
-    public Intake (DcMotorEx intaker) {
+    public Intake(DcMotorEx intaker) {
         super();
         motors.add(intaker);
     }
 
     public void update(Gamepad gp1, Gamepad gp2) {
         if (gp1.right_trigger >= 0.5) { //If right trigger is pressed
-            goingin = true; //Set direction: In
-        } else {
+            goingin = true; //Set direction to in
+        } else if (gp1.left_trigger >= 0.5) { //Otherwise if left trigger is pressed
+            goingout = true; //Set direction to out
+        } else { //If neither is pressed
+            //Set direction to none
             goingin = false;
-        }
-        if (gp1.left_trigger >= 0.5) { //Otherwise if left trigger is pressed
-            goingout = true; //Set direction: Out
-        } else {
             goingout = false;
+            //-----
         }
     }
 
+
     public void write() {
-        if (goingin) { //If direction is In
+        if (goingin) { //If direction is set to in
             motors.get(0).setPower(1); //Turn on the intake
-        } else if (goingout) { //If direction is Out
+        } else if (goingout) { //If direction is set to out
             motors.get(0).setPower(-1); //Reverse the intake
         } else { //If no direction is set
             motors.get(0).setPower(0); //Stop the intake
