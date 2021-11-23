269c38ac2ccb343caf26d515afca1f620fc0036e
BenjaminBeseler <23besebe@students.lovelandschools.org>
2021-11-22 19:58:01 -0500

    Location and more starter auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Auto.java
@@ -2,15 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Location;
 
 @Autonomous(name="Starter Auto", group="Autonomous")
 public class Starter_Auto extends LinearOpMode {
     private Robot robo;
-
+    private Intake intaker;
     //private Drivetrain drivey;
+
+
+    private final Location Tester = new Location(100, 0, 100, 100);
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        robo = new Robot (this);
+        intaker = new Intake((DcMotorEx) hardwareMap.get(DcMotor.class, "intake"));
+
+
+        waitForStart();
 
     }
 }
