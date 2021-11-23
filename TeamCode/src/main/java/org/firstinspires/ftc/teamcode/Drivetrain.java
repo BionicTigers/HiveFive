21af4846af71ea7769859561e2ea6bb84f4372de
Benjamin Beseler <23besebe@students.lovelandschools.org>
2021-11-22 20:36:37 -0500

    Scary thing be scary. bug say untracked files prevent merge and we don't like that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -18,7 +18,7 @@ public class Drivetrain extends Mechanism {
     public double[] motorPowers;
     public int[] motorIndices;
     public Telemetry telemetree;
-    private Odometry odo;
+    //private Odometry odo;
     private Location forward = new Location (0, 0, 1000, 0);
     private Location backward = new Location (0, 0, -1000, 0);
     private Location left = new Location (-500, 0, 0, 0);
@@ -37,7 +37,7 @@ public class Drivetrain extends Mechanism {
         robot = bot;
         motorIndices = motorNumbers;
         telemetree = T;
-        odo = bot.odometry;
+        //odo = bot.odometry;
         getServos().add(servo);
 
         for (int motNum : motorNumbers) {
@@ -47,58 +47,58 @@ public class Drivetrain extends Mechanism {
         }
         motorPowers = new double[]{0, 0, 0, 0};
     }
-        //sets the motorNumbers array based on input from joysticks
-        public void determineMotorPowers (Gamepad driverPad){
-            double P = Math.hypot(-driverPad.left_stick_x, -driverPad.left_stick_y);
-            double robotAngle = Math.atan2(-driverPad.left_stick_y, -driverPad.left_stick_x);
-            double rightX = driverPad.right_stick_x;
+    //sets the motorNumbers array based on input from joysticks
+    public void determineMotorPowers (Gamepad driverPad){
+        double P = Math.hypot(-driverPad.left_stick_x, -driverPad.left_stick_y);
+        double robotAngle = Math.atan2(-driverPad.left_stick_y, -driverPad.left_stick_x);
+        double rightX = driverPad.right_stick_x;
 
-            double sinRAngle = Math.sin(robotAngle);
-            double cosRAngle = Math.cos(robotAngle);
+        double sinRAngle = Math.sin(robotAngle);
+        double cosRAngle = Math.cos(robotAngle);
 
-            final double v1 = (P * sinRAngle) - (P * cosRAngle) + rightX;  //frontRight
-            final double v2 = (P * sinRAngle) + (P * cosRAngle) - rightX;  //frontLeft
-            final double v3 = (P * sinRAngle) + (P * cosRAngle) + rightX;  //backRight
-            final double v4 = (P * sinRAngle) - (P * cosRAngle) - rightX;  //backLeft
+        final double v1 = (P * sinRAngle) - (P * cosRAngle) + rightX;  //frontRight
+        final double v2 = (P * sinRAngle) + (P * cosRAngle) - rightX;  //frontLeft
+        final double v3 = (P * sinRAngle) + (P * cosRAngle) + rightX;  //backRight
+        final double v4 = (P * sinRAngle) - (P * cosRAngle) - rightX;  //backLeft
 
-            motorPowers[0] = v1;
-            motorPowers[1] = v2;
-            motorPowers[2] = v3;
-            motorPowers[3] = v4;
+        motorPowers[0] = v1;
+        motorPowers[1] = v2;
+        motorPowers[2] = v3;
+        motorPowers[3] = v4;
+    }
+    public void determineServoMovement(Gamepad driverPad){
+        if (driverPad.a){
+            servos.get(0).setPosition(0.6);
+
+        } else if(driverPad.b){
+            servos.get(0).setPosition(0.67);
         }
-        public void determineServoMovement(Gamepad driverPad){
-            if (driverPad.a){
-                servos.get(0).setPosition(0.6);
+    }
+    //Uses values from motor powers array to move the robot
+    public void robotMovement () {
 
-            } else if(driverPad.b){
-                servos.get(0).setPosition(0.67);
-            }
-        }
-        //Uses values from motor powers array to move the robot
-        public void robotMovement () {
-
-        }
-        public void update (Gamepad gp1, Gamepad gp2){
+    }
+    public void update (Gamepad gp1, Gamepad gp2){
             /*
             All telemetree commands implement telemetry to allow the driver to view motor powers
             while code is active
              */
-            telemetree.addLine("Motor Powers");
-            telemetree.addData("Front Right Power", motorPowers[0]);
-            telemetree.addData("Front Left Power", motorPowers[1]);
-            telemetree.addData("Back Right Power", motorPowers[2]);
-            telemetree.addData("Back Left Power", motorPowers[3]);
-            telemetree.update();
-            determineMotorPowers(gp1); //Updates values in motorPowers array
-            determineServoMovement(gp1);
-        }
-
-        public void write () {
-            //Sets the motor powers based on the determineMotorPowers() method that was run in the update() method
-            int i = 0;
-            for (DcMotorEx motor : motors.subList(motorIndices[0], motorIndices[3] + 1)) {
-                motor.setPower(motorPowers[i]);
-                i++;
-           }
-       }
+        telemetree.addLine("Motor Powers");
+        telemetree.addData("Front Right Power", motorPowers[0]);
+        telemetree.addData("Front Left Power", motorPowers[1]);
+        telemetree.addData("Back Right Power", motorPowers[2]);
+        telemetree.addData("Back Left Power", motorPowers[3]);
+        telemetree.update();
+        determineMotorPowers(gp1); //Updates values in motorPowers array
+        determineServoMovement(gp1);
     }
+
+    public void write () {
+        //Sets the motor powers based on the determineMotorPowers() method that was run in the update() method
+        int i = 0;
+        for (DcMotorEx motor : motors.subList(motorIndices[0], motorIndices[3] + 1)) {
+            motor.setPower(motorPowers[i]);
+            i++;
+        }
+    }
+}
