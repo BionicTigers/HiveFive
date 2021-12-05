8165871f9c2d90bd742293e74d6b1674e2c6c1ab
Benjamin Beseler <23besebe@students.lovelandschools.org>
2021-12-05 17:27:01 -0500

    Scary thing be scary. bug say untracked files prevent merge and we don't like that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainTesting.java
@@ -18,6 +18,11 @@ public class DrivetrainTesting extends LinearOpMode{
     public String[] motorNames = {"frontRight","frontLeft","backLeft","backRight"}; //establishes motor names
     public Drivetrain drivetrain; //declares drivetrain
     public Robot robot; //declares robot
+    public Intake intake;
+    public Transfer transfer;
+    public Output output;
+    public Cap cap;
+    public Spinner spinner;
 
     public int[] motorNumbers = {0, 1, 2, 3}; //creates motor numbers array
 
@@ -28,34 +33,44 @@ public class DrivetrainTesting extends LinearOpMode{
         robot.initMotors(motorNames);
         waitForStart();
         //establishes IMU parameters/variables
-        BNO055IMU imu;
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-        parameters.loggingEnabled = true;
-        parameters.loggingTag = "IMU";
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
+//        BNO055IMU imu;
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+//        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+//        parameters.loggingEnabled = true;
+//        parameters.loggingTag = "IMU";
+//        imu = hardwareMap.get(BNO055IMU.class, "imu");
+//        imu.initialize(parameters);
 
         //what runs constantly once play button is pressed
         while(opModeIsActive()) {
             drivetrain.update(gamepad1, gamepad2);
+            intake.update(gamepad1, gamepad2);
+            transfer.update(gamepad1, gamepad2);
+            output.update(gamepad1, gamepad2);
+            cap.update(gamepad1, gamepad2);
+            spinner.update(gamepad1, gamepad2);
             drivetrain.write();
+            intake.write();
+            transfer.write();
+            output.write();
+            cap.write();
+            spinner.write();
             //Returns values of angles, velocity, and acceleration on each axis
-            telemetry.addLine("IMU Data");
-            telemetry.addData("orientation:", "Angle:x=%6.1f,y=%6.1f,z=%6.1f",
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle,
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).secondAngle,
-                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
-            telemetry.addData("Velocity", "Vel: x=%6.1f,y=%6.1f,z=%6.1f",
-                    imu.getVelocity().xVeloc,
-                    imu.getVelocity().yVeloc,
-                    imu.getVelocity().zVeloc);
-            telemetry.addData("LinearAccel", "Dist: x=%6.1f,y=%6.1f,z=%6.1f",
-                    imu.getLinearAcceleration().xAccel,
-                    imu.getLinearAcceleration().yAccel,
-                    imu.getLinearAcceleration().zAccel);
+//            telemetry.addLine("IMU Data");
+//            telemetry.addData("orientation:", "Angle:x=%6.1f,y=%6.1f,z=%6.1f",
+//                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle,
+//                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).secondAngle,
+//                    imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
+//            telemetry.addData("Velocity", "Vel: x=%6.1f,y=%6.1f,z=%6.1f",
+//                    imu.getVelocity().xVeloc,
+//                    imu.getVelocity().yVeloc,
+//                    imu.getVelocity().zVeloc);
+//            telemetry.addData("LinearAccel", "Dist: x=%6.1f,y=%6.1f,z=%6.1f",
+//                    imu.getLinearAcceleration().xAccel,
+//                    imu.getLinearAcceleration().yAccel,
+//                    imu.getLinearAcceleration().zAccel);
             telemetry.update();
         }
     }
