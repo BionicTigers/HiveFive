07bd059fe31fca9e2ff94a074a727d1366068a5d
Benjamin Beseler <23besebe@students.lovelandschools.org>
2021-12-06 19:15:03 -0500

    Scary thing be scary. bug say untracked files prevent merge and we don't like that

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionalTransfer.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class PositionalTransfer extends Mechanism{
+    boolean up;
+    boolean down;
+
+    public PositionalTransfer(DcMotorEx motor){
+        super();
+        motors.add(motor);
+    }
+
+    public void update(Gamepad gp1, Gamepad gp2){
+        if(gp2.right_trigger >= 0.5){
+            up = true;
+        } else if(gp2.left_trigger >= 0.5){
+            down = true;
+        } else{
+            up = false;
+            down = false;
+        }
+    }
+
+    public void write(){
+        if(up){
+            motors.get(0).setTargetPosition(1);
+        } else if(down){
+            motors.get(0).setTargetPosition(0);
+        }
+    }
+
+}
