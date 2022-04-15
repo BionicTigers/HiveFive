692fd6aebb336a946e0faaf48aa395d938077510
jackgerber <youdontknowjack747@gmail.com>
2022-04-14 20:15:35 -0400

    Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsAutos/RedsideWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsAutos/RedsideWarehouse.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.WorldsAutos;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.teamcode.Location;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.Spinner;
 import org.firstinspires.ftc.teamcode.Turret;
+import org.firstinspires.ftc.teamcode.Cap;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,7 +30,7 @@ public class RedsideWarehouse extends LinearOpMode {
     public Drivetrain drivetrain; //declares drivetrain
     public Robot robot; //declares robot
     public Intake intake;
-    //public Cap cap;
+    public Cap cap;
     public Spinner spinner;
     public Turret turret;
     public int[] motorNumbers = {0, 1, 2, 3}; //creates motor numbers array
@@ -41,7 +43,7 @@ public class RedsideWarehouse extends LinearOpMode {
 
     public boolean hasFreight = false;
 
-    private final Location dropZone = new Location(465.544,0,0,0);
+    private final Location dropZone = new Location(-465.544,0,0,0);
     private final Location wall = new Location(10,0,0,0);
     private final Location warehouse = new Location(10,0,500,0);
     private final Location grabZone = new Location(10,0,800,0);
@@ -52,12 +54,13 @@ public class RedsideWarehouse extends LinearOpMode {
         drivetrain = new Drivetrain(robot, motorNumbers, telemetry, hardwareMap.get(Servo.class, "SDriveL"), hardwareMap.get(Servo.class, "SDriveM"), hardwareMap.get(Servo.class, "SDriveR"));
         intake = new Intake(hardwareMap.get(DcMotorEx.class, "intakeMotor"), hardwareMap.get(Servo.class, "intake2"));
         spinner = new Spinner(hardwareMap.get(DcMotorEx.class,"spinner"));
-        //cap = new Cap(hardwareMap.get(CRServo.class, "cap1"), hardwareMap.get(Servo.class, "cap2"));
+        cap = new Cap(hardwareMap.get(CRServo.class, "cap1"), hardwareMap.get(Servo.class, "cap2"));
         turret = new Turret(hardwareMap.get(DcMotorEx.class, "turretSpin"), hardwareMap.get(DcMotorEx.class, "turretLift"), hardwareMap.get(Servo.class, "turretLeft"), hardwareMap.get(Servo.class, "turretRight"), telemetry);
         //color = hardwareMap.get(ColorSensor.class, "color");
         robot.initMotors(motorNames);
 
         Deadline park = new Deadline(25, TimeUnit.SECONDS);
+        cap.getServos().get(0).setPosition(0.5);
 
         waitForStart();
         while (!park.hasExpired()) {
@@ -72,17 +75,17 @@ public class RedsideWarehouse extends LinearOpMode {
             turret.motors.get(1).setTargetPosition(2800);
             turret.motors.get(0).setTargetPosition(2590);
             sleep(150);
-            drivetrain.moveToPosition(wall, 5, 5, 2);
-            drivetrain.moveToPosition(warehouse, 5, 5, 2);
+            //drivetrain.moveToPosition(wall, 5, 5, 2);
+            //drivetrain.moveToPosition(warehouse, 5, 5, 2);
             intake.intake();
             turret.servos.get(0).setPosition(0.8);
             turret.servos.get(1).setPosition(0.2);
-            drivetrain.moveToPosition(grabZone, 5, 5, 2);
+            //drivetrain.moveToPosition(grabZone, 5, 5, 2);
             while (!hasFreight && opModeIsActive()) {
-                drivetrain.motors.get(0).setPower(30);
-                drivetrain.motors.get(1).setPower(30);
-                drivetrain.motors.get(2).setPower(30);
-                drivetrain.motors.get(3).setPower(30);
+                //drivetrain.motors.get(0).setPower(30);
+                //drivetrain.motors.get(1).setPower(30);
+                //drivetrain.motors.get(2).setPower(30);
+                //drivetrain.motors.get(3).setPower(30);
                 if (distance.getDistance(DistanceUnit.CM) < 0.8) {
                     hasFreight = true;
                 }
@@ -91,13 +94,13 @@ public class RedsideWarehouse extends LinearOpMode {
             intake.motors.get(0).setPower(0);
             turret.servos.get(0).setPosition(0.5);
             turret.servos.get(1).setPosition(0.5);
-            drivetrain.moveToPosition(wall, 5, 5, 2);
+            //drivetrain.moveToPosition(wall, 5, 5, 2);
+        }
 
-        drivetrain.moveToPosition(wall,5,5,2);
-        drivetrain.moveToPosition(warehouse,5,5,2);
+        //drivetrain.moveToPosition(wall,5,5,2);
+        //drivetrain.moveToPosition(warehouse,5,5,2);
         }
     }
-}
 
 //Depositing freight position
 //465.544, 0, 0
